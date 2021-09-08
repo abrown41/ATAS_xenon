f4d8fad86a8a5b5f87e71dd39c51002900177f61
Andrew Brown <andrew.brown@qub.ac.uk>
2021-09-08 10:21:46 +0100

    modified pltfits to take optional filename argument

--- bin/pltfits.py
+++ bin/pltfits.py
@@ -6,18 +6,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser as AP
 import pandas as pd
+from pathlib import Path
 
 def read_command_line():
     parser = AP()
-    parser.add_argument('files', nargs="+",help="list of directories containing output for plotting")
+    parser.add_argument('dirs', nargs="+",help="list of directories containing output for plotting")
+    parser.add_argument('-f','--file',help="filename of fitting parameters csv, default= 'fitparmams.csv'", default='fitparams.csv')
     parser.add_argument('-e','--exp', nargs="+",help="csv files containing experimental data",default=None)
     parser.add_argument('-p','--param', type=str,help="parameter for plotting: default is z0 (options phi, gam)",default="z0")
     return vars(parser.parse_args())
 
 args=read_command_line()
 
-for f in args["files"]:
-    df = pd.read_csv(f+"/fitparams.csv")
+for f in args["dirs"]:
+    p=Path(f)
+    df = pd.read_csv(p / args["file"])
     plt.plot(df["t"],df[args["param"]],label=f)
 
 #count = 0 
