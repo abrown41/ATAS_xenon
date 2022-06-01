7ba7261db0eae77245b200509dd6ac67dc55a3b2
lhutcheson <lhutcheson02@qub.ac.uk>
2022-06-01 15:09:30 +0100

    Update shell script for setting up RMT calculations
    Former-commit-id: d0457f948a86f989a0adbd86fd06ca8f5a6e5e96

--- archer_files/builddirs.sh
+++ archer_files/builddirs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 if [[ $1 == "" ]] ; then echo "STOP: enter intensity" ; exit 1 ; fi
-sed s:INTENS:$1:g ../input.conf >input.conf
 
-#mkdir delay_{-,+}_{00..05}.{00,25,50,75}
-#mkdir delay_+_{06..07}.{00,25,50,75}
-mkdir delay_-_{00..04}.{125,375,625,875}
+mkdir delay_{-,+}_{0..5}.{00,25,50,75}
 
-for file in delay_-_{00..04}.{125,375,625,875} ; do ( cd $file ; mkdir ground state data ; ln -s /work/e585/e585/abrown/ATAS/inner/L48/* . ; ln -s $CRAY/bin/rmt.x . ) ; done
+for file in delay_+_{0..5}.{00,25,50,75} ; do ( cd $file ; cp ../input.conf . ; mkdir ground state data ; ln -s $NR_Xe_lmax_40/RMTinput/* . ; ln -s $CRAY_master/bin/rmt.x . ) ; done
+for file in delay_-_{0..5}.{00,25,50,75} ; do ( cd $file ; cp ../input.conf . ; mkdir ground state data ; ln -s $NR_Xe_lmax_40/RMTinput/* . ; ln -s $CRAY_master/bin/rmt.x . ) ; done
 
-for file in delay_-_{00..04}.{125,375,625,875}; do delay=$(echo $file | sed s:delay::g | sed s:_::g) ; sed s:DUMMY:$delay:g input.conf > $file/input.conf ; done
+for delay in $(seq 0.00 0.25 5.75) ; do (cd delay_-_$delay ; ./gaussian.x -$delay $1 ) ; done
+for delay in $(seq 0.00 0.25 5.75) ; do (cd delay_+_$delay ; ./gaussian.x $delay $1 ) ; done
