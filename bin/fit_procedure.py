8810b44d7cc273884dd7ae9015e2a49645563396
Andrew Brown <andrew.brown@qub.ac.uk>
2022-06-07 14:37:39 +0100

    Moved construction of OD to function. OD now stored as dataframe
    Former-commit-id: 790c54ee0d9f345a1a7843c9e892d08b8c43cd08

--- bin/fit_procedure.py
+++ bin/fit_procedure.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Uses dipoleX.X.csv and fieldX.X.csv (output from makedf.py) to calculate the OD 
-for a single IR intensity at each time delay in a time delay scan, perform 
-the fitting procedure and save the time delay scan, the fits at each time
-delay and the fitting parameters to .csv files named ODX.X.csv, OD_fitX.X.csv 
-and fit_paramsX.X.csv respectively. 'X.X' is the IR intensity.
+Uses dipoleX.X.csv and fieldX.X.csv (output from makedf.py) to calculate the OD
+for a single IR intensity at each time delay in a time delay scan, perform the
+fitting procedure and save the time delay scan, the fits at each time delay and
+the fitting parameters to .csv files named ODX.X.csv, OD_fitX.X.csv and
+fit_paramsX.X.csv respectively. 'X.X' is the IR intensity.
 
-In calculating the OD, the dipole is tapered with an exponential decay to simulate
-Auger Decay.
+In calculating the OD, the dipole is tapered with an exponential decay to
+simulate Auger Decay.
 
 Executed from the directory containing the dipole and electric field files by:
     python fit_procedure.py -i <IR intensity>
@@ -16,9 +16,9 @@ Where the argument -i chooses the IR intensity.
 Adapted from a script by Max.
 """
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                    Imports
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy.constants import physical_constants as constants
@@ -27,190 +27,201 @@ import matplotlib.pyplot as plt
 import helper_functions as hf
 
 args = hf.read_command_line()
-intensity=args["IR_intensity"]
+intensity = args["IR_intensity"]
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   Constants
-#-----------------------------------------------------------------------
-gamma_xe1 = 0.122 # from literature (Anderson 2001 I think?)
+# -----------------------------------------------------------------------
+gamma_xe1 = 0.122  # from literature (Anderson 2001 I think?)
 alpha = constants['fine-structure constant'][0]
 
 # set region of interest for fit
-roi = slice(*hf.get_roi(hf.get_energy_axis(),erange=(60,62)))
+roi = slice(*hf.get_roi(hf.get_energy_axis(), erange=(60, 62)))
 photonenergy = hf.get_energy_axis()[roi]
 
-#-----------------------------------------------------------------------
-#                   Fit setup
-#-----------------------------------------------------------------------
-lower_bounds = [1e-6,  -np.pi, 0.5*gamma_xe1] + [-1]
-upper_bounds = [np.inf, np.pi, 2*gamma_xe1] + [1]
-bounds = (lower_bounds, upper_bounds)
+# -----------------------------------------------------------------------
+#                   Read Data
+# -----------------------------------------------------------------------
 
-p_init = np.array([1, 0, gamma_xe1] + [0])
-params = []
-errors = []
-#-----------------------------------------------------------------------
-#                   Read Data 
-#-----------------------------------------------------------------------
+def getOD(intensity):
 
-df1 = pd.read_csv("dipole"+str(intensity)+".csv")
-df2 = pd.read_csv("field"+str(intensity)+".csv")
+    df1 = pd.read_csv(f"dipole{intensity}.csv")
+    df2 = pd.read_csv(f"field{intensity}.csv")
 
-#-----------------------------------------------------------------------
-#                   Delay loop 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+#                   Delay loop
+# -----------------------------------------------------------------------
 
 # Get delays
-delay_strings = df1.columns.values[1:]
-td = delay_strings.astype(float)
-OD_sim = []
+    delay_strings = df1.columns.values[1:]
+#    td = delay_strings.astype(float)
+    OD_sim = pd.DataFrame()
 
 # prepad signal with zeros to improve resolution
-file_length=len(df1)
-slen = 2**21
-nzero = slen - file_length
+    file_length = len(df1)
+    slen = 2**21
+    nzero = slen - file_length
 
-for delay in delay_strings:
-    t_zero = 1 + float(delay) + 5.75
+    for delay in delay_strings:
+        t_zero = 1 + float(delay) + 5.75
 
-    xuv_dipole = df1[delay]
-    time = hf.au_to_fs(df1['Time']) - t_zero
-    efield = df2[delay]
+        xuv_dipole = df1[delay]
+        time = hf.au_to_fs(df1['Time']) - t_zero
+        efield = df2[delay]
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   OD with exponential decay
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
-    # decay setup
-    lifetime = hf.au_to_fs(1/(hf.ev_to_au(gamma_xe1)/2))
-    decay = np.exp(- time/lifetime)
-    decay[time<0] = 1
+        # decay setup
+        lifetime = hf.au_to_fs(1/(hf.ev_to_au(gamma_xe1)/2))
+        decay = np.exp(- time/lifetime)
+        decay[time < 0] = 1
 
-    # blackman window
-    # window = np.blackman(file_length)
+        # blackman window
+        # window = np.blackman(file_length)
 
-    # prepad to improve FT resolution
-    prepad = np.zeros(nzero)
-    field      = np.concatenate((prepad,  efield))
-    dipole_pad = np.concatenate((prepad, decay * xuv_dipole))
+        # prepad to improve FT resolution
+        prepad = np.zeros(nzero)
+        field = np.concatenate((prepad,  efield))
+        dipole_pad = np.concatenate((prepad, decay * xuv_dipole))
 
-    # FT to frequency domain
-    dw = np.fft.fft(dipole_pad)
-    ew = np.fft.fft(field)
-    
-    t_mult = time[1] - time[0]
-    w = np.fft.fftfreq(field.shape[0], hf.fs_to_au(t_mult)/(2*np.pi))
-    w_ev = hf.au_to_ev(w)
-    w_roi = w_ev[np.all([(w_ev > 59), (w_ev < 62)], axis=0)]
-    
-    OD = -4*np.pi*alpha*w*np.imag(dw/ew)
-    OD_roi = OD[np.all([(w_ev > 59), (w_ev < 62)], axis=0)]
-    OD_sim.append(OD_roi)
+        # FT to frequency domain
+        dw = np.fft.fft(dipole_pad)
+        ew = np.fft.fft(field)
 
-#-----------------------------------------------------------------------
-#                   Fit 
-#-----------------------------------------------------------------------
-    
+        t_mult = time[1] - time[0]
+        w = np.fft.fftfreq(field.shape[0], hf.fs_to_au(t_mult)/(2*np.pi))
+        w_ev = hf.au_to_ev(w)
+        w_roi = w_ev[np.all([(w_ev > 59), (w_ev < 62)], axis=0)]
+
+        OD = -4*np.pi*alpha*w*np.imag(dw/ew)
+        OD_roi = OD[np.all([(w_ev > 59), (w_ev < 62)], axis=0)]
+        OD_sim[delay] = OD_roi
+
+    return w_roi, OD_sim
+
+# -----------------------------------------------------------------------
+#                   Fit
+# -----------------------------------------------------------------------
+
+
+w_roi, OD_sim = getOD(intensity)
+
+
+# -----------------------------------------------------------------------
+#                   Fit setup
+# -----------------------------------------------------------------------
+# fit parameters:[line_strength, phase, line_width, constant_background_term)
+#              [  z0,   ϕ,     Γ,             c]
+lower_bounds = [1e-6,  -np.pi, 0.5*gamma_xe1, -1]
+upper_bounds = [np.inf, np.pi, 2*gamma_xe1, 1]
+bounds = (lower_bounds, upper_bounds)
+
+# Initial guess for fitting parameters 
+#                [z0, ϕ,   Γ,       c]
+p_init = np.array([1, 0, gamma_xe1, 0])
+params = []
+errors = []
+
+for delay in OD_sim.columns:
     # restrict fit parameters to resonable values
     p_init[:-1:3] = abs(p_init[:-1:3])
-    p_init[2:-1:3] = abs(p_init[2:-1:3])    
+    p_init[2:-1:3] = abs(p_init[2:-1:3])
 
-    popt, pcov = curve_fit(hf.fit_lineshapes, w_roi, OD_roi, 
-                             p_init, maxfev=1000000, bounds=bounds)
+    popt, pcov = curve_fit(hf.fit_lineshapes, w_roi, OD_sim[delay],
+                           p_init, maxfev=1000000, bounds=bounds)
 
     params.append(popt)
     errors.append(np.sqrt(np.abs(np.diag(pcov))))
-    
+
     # reuse fit result in next iteration:
     # p_init = popt
-    
+
 params = np.array(params)
 errors = np.array(errors)
 
-#%%
-#-----------------------------------------------------------------------
+# %%
+# -----------------------------------------------------------------------
 #                   Plots
-#-----------------------------------------------------------------------
-td*=-1
-fig, (ax1,ax2,ax3) = plt.subplots(1, 3)
+# -----------------------------------------------------------------------
+td = [-float(delay) for delay in OD_sim.columns]
+#td *= -1
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
-ax1.plot(td, params[:,0])
+ax1.plot(td, params[:, 0])
 ax1.set_title('Line strength')
 
-ax2.plot(td, params[:,1])
+ax2.plot(td, params[:, 1])
 ax2.set_title('Phase (rad)')
 ax2.set_xlabel('Time delay (fs)')
 
-ax3.plot(td, params[:,2])
+ax3.plot(td, params[:, 2])
 ax3.set_title('Line width (eV)')
 
 plt.figure(3)
-plt.plot(td, params[:,3])
+plt.plot(td, params[:, 3])
 plt.ylabel('Background')
 plt.xlabel('Time delay (fs)')
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   Image
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
-paramdict = {'cmap':'jet', 'shading':'nearest'}
-fontdict = {'color':'w', 'fontsize':10}
+paramdict = {'cmap': 'jet', 'shading': 'nearest'}
+fontdict = {'color': 'w', 'fontsize': 10}
 
-OD_sim = np.array(OD_sim)
+#OD_sim = np.array(OD_sim)
 OD_fit = np.zeros((params.shape[0], w_roi.size))
 for p, popt in enumerate(params):
     OD_fit[p] = hf.fit_lineshapes(w_roi, *popt)
 
 
-fig, ax = plt.subplots(nrows=1,ncols=2, num=5)
+fig, ax = plt.subplots(nrows=1, ncols=2, num=5)
 fig.subplots_adjust(right=0.9, left=0.1, top=0.9, bottom=0.15, wspace=0.2)
 
-im = ax[0].pcolor(w_roi-5.5, td, OD_sim, **paramdict, vmin=-0.04, vmax=0.25)
+im = ax[0].pcolor(w_roi-5.5, td, OD_sim.transpose(), **paramdict, vmin=-0.04, vmax=0.25)
 vmin, vmax = im.get_clim()
 im2 = ax[1].pcolor(w_roi-5.5, td, OD_fit, **paramdict, vmin=vmin, vmax=vmax)
 
 cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.75])
 cbar = fig.colorbar(im2, cax=cbar_ax)
 cbar_ax.set_title('OD')
-cbar.set_ticks([0,0.1,0.2])
+cbar.set_ticks([0, 0.1, 0.2])
 
 ax[0].set_ylabel(r'Time delay (fs)')
 ax[0].set_xlabel(r'Photon energy (eV)')
 ax[1].set_xlabel(r'Photon energy (eV)')
 
-ax[0].set_title('Simulation', fontsize=12, pad=1)    
-ax[1].set_title('Fit', fontsize=12, pad=1)  
+ax[0].set_title('Simulation', fontsize=12, pad=1)
+ax[1].set_title('Fit', fontsize=12, pad=1)
 
 plt.show()
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   Save Time Delay Scan + Fit
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
-OD_df = pd.DataFrame()
-OD_df['Energy'] = w_roi
-for col in range(len(OD_sim[:,0])):
-    delay = str(td[col])
-    OD_df[delay] = OD_sim[col,:]
+OD_sim.insert(loc=0, column='Energy', value=w_roi)
 
-OD_df.to_csv('OD'+str(intensity)+'.csv', index=False)
+OD_sim.to_csv(f'OD{intensity}.csv', index=False)
 
 fit_df = pd.DataFrame()
 fit_df['Energy'] = w_roi
-for col in range(len(OD_fit[:,0])):
+for col in range(len(OD_fit[:, 0])):
     delay = str(td[col])
-    fit_df[delay] = OD_fit[col,:]
+    fit_df[delay] = OD_fit[col, :]
 
-fit_df.to_csv('OD_fit'+str(intensity)+'.csv', index=False)
+fit_df.to_csv(f'OD_fit{intensity}.csv', index=False)
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   Save Fitting Parameters
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 params_df = pd.DataFrame()
 params_df['Time Delays'] = td
-params_df['Line Strength'] = params[:,0]
-params_df['Phase'] = params[:,1]
-params_df['Line Width'] = params[:,2]
+params_df['Line Strength'] = params[:, 0]
+params_df['Phase'] = params[:, 1]
+params_df['Line Width'] = params[:, 2]
 
-params_df.to_csv('fit_params'+str(intensity)+'.csv', index=False)
+params_df.to_csv('fit_params{intensity}.csv', index=False)
--- bin/helper_functions.py
+++ bin/helper_functions.py
@@ -24,17 +24,18 @@ from argparse import ArgumentParser as AP
 from pathlib import Path
 
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   Constants
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 pldp_au = 0.77              # path-length-density-product in atomic units
 alpha = cnt.constants.fine_structure
 lineshape_constant = pldp_au/np.log(10)*4*np.pi*alpha
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 #                   Functions
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
 
 def au_to_fs(time_in_au):
     """
@@ -105,8 +106,8 @@ def ev_to_au(energy_in_ev):
 
 def cos2_window(index1, index2, width, size):
 
-    indmax = np.maximum(index1,index2)
-    indmin = np.minimum(index1,index2)
+    indmax = np.maximum(index1, index2)
+    indmin = np.minimum(index1, index2)
 
     arr1 = np.sin(np.pi/2/width*np.array(range(width)))**2.
     arr2 = np.ones((indmax-indmin))
@@ -116,13 +117,13 @@ def cos2_window(index1, index2, width, size):
     arr4 = np.zeros((checksum))
 
     if np.greater_equal(checksum, 0.):
-        return np.roll(np.concatenate((arr1,arr2,arr3,arr4)), indmin-width)
+        return np.roll(np.concatenate((arr1, arr2, arr3, arr4)), indmin-width)
 
     else:
         print('Cos2 Window error. Sizes not okay.')
         return np.zeros((size))
 
-    
+
 def taper_cos2(width, array):
 
     number_of_zeros = int(width / 100.)
@@ -132,34 +133,36 @@ def taper_cos2(width, array):
     if not width == 0:
         taper_mod = np.arange(1, width + 1)
         taper_mod = np.cos(np.pi / (2.0 * width) * taper_mod) ** 2
-        modulation[array.shape[0] - (width + number_of_zeros):array.shape[0] - number_of_zeros] = taper_mod
+        modulation[array.shape[0] - (width + number_of_zeros)
+                                     :array.shape[0] - number_of_zeros] = taper_mod
         modulation[number_of_zeros:width + number_of_zeros] = taper_mod[::-1]
 
     modulation[array.shape[0] - number_of_zeros:] = 0.0
     modulation[0:number_of_zeros] = 0.0
     return modulation
 
-    
 
 def TDDM_Reconstruction(data, ax_off, ax_mult):
 
-    if len(data.shape)==1:
+    if len(data.shape) == 1:
 
-        p_data = np.concatenate((np.zeros(int(ax_off/ax_mult)),data))
+        p_data = np.concatenate((np.zeros(int(ax_off/ax_mult)), data))
 
-        axis = au_to_fs(np.fft.fftfreq(p_data.shape[0], ev_to_au(ax_mult)/(2*np.pi)))
+        axis = au_to_fs(np.fft.fftfreq(
+            p_data.shape[0], ev_to_au(ax_mult)/(2*np.pi)))
 
         tddm = np.fft.fft(p_data, norm='ortho')
 
         return (tddm, axis)
 
-    if len(data.shape)==2:
+    if len(data.shape) == 2:
 
-        zeros_front = np.zeros((int(ax_off/ax_mult),data.shape[1]))
+        zeros_front = np.zeros((int(ax_off/ax_mult), data.shape[1]))
 
         p_data = np.concatenate((zeros_front, data), axis=0)
 
-        axis = au_to_fs(np.fft.rfftfreq(p_data.shape[0], ev_to_au(ax_mult)/(2*np.pi)))
+        axis = au_to_fs(np.fft.rfftfreq(
+            p_data.shape[0], ev_to_au(ax_mult)/(2*np.pi)))
 
         tddm = np.zeros_like(p_data, dtype=complex)
 
@@ -168,17 +171,14 @@ def TDDM_Reconstruction(data, ax_off, ax_mult):
         return (tddm, axis)
 
 
-
 def get_energy_axis():
 
-    your_energy_axis = np.linspace(51.012496,67.000271,1962)
+    your_energy_axis = np.linspace(51.012496, 67.000271, 1962)
 
     return your_energy_axis
 
 
-
 def getOD(intensity):
-
     """ Returns a whole time delay scan at a given intensity.
 
         Shape (td_axis_size, energy_axis_size). """
@@ -192,45 +192,39 @@ def getOD(intensity):
     return df.transpose().values, time_delay_axis
 
 
-
 def get_intensities():
 
-    return [1.3,1.6,1.9,2.2,2.5]
+    return [1.3, 1.6, 1.9, 2.2, 2.5]
 
 
+def get_roi(energy_axis, erange=(60, 62)):
 
-def get_roi(energy_axis,erange=(60,62)):
+    for i, e in enumerate(energy_axis):
 
-    for i,e in enumerate(energy_axis): 
-
-        if e>erange[0]:
+        if e > erange[0]:
 
             first = i
 
             break
 
-    for i,e in enumerate(energy_axis): 
+    for i, e in enumerate(energy_axis):
 
-        if e>erange[1]:
+        if e > erange[1]:
 
             last = i
 
             break
 
-    return (first,last)
-
+    return (first, last)
 
 
 def wrap(phase, offset=0):
-
     """ Opposite of np.unwrap. Restrict phase to [-2*pi, 2*pi]. """
 
-    return ( phase + np.pi + offset) % (2 * np.pi ) - np.pi - offset
-
+    return (phase + np.pi + offset) % (2 * np.pi) - np.pi - offset
 
 
 def DCM_lineshape(energy_axis, z, phi, resonance_energy, gamma):
-
     """
 
     Dipole control model (DCM) line shape function for a single absorption line
@@ -263,27 +257,24 @@ def DCM_lineshape(energy_axis, z, phi, resonance_energy, gamma):
 
     """
 
-    lineshape = (gamma/2*np.cos(phi) - (energy_axis-resonance_energy)*np.sin(phi)) / ((energy_axis-resonance_energy)**2 + gamma**2/4)
+    lineshape = (gamma/2*np.cos(phi) - (energy_axis-resonance_energy)
+                 * np.sin(phi)) / ((energy_axis-resonance_energy)**2 + gamma**2/4)
 
-    return  z * lineshape
-
+    return z * lineshape
 
 
 # resonance energies after calibration
 
 # (retrieved by fitting a Lorentzian to the spectra far out of temporal overlap)
-
 e_res = [60.88]
 
 
-
 def fit_lineshapes(energy_axis, *params):
-
     """
 
-    Fit function to extract line shape parameters from several absorption lines 
+    Fit function to extract line shape parameters from several absorption lines
 
-    from the measurement data. I omitted the convolution with the experimental 
+    from the measurement data. I omitted the convolution with the experimental
 
     spectral resolution which only marginally affects the fit results anyway.
 
@@ -321,53 +312,62 @@ def fit_lineshapes(energy_axis, *params):
 
     gamma = params[2:-1:3]
 
-    for e, energy in [(0, e_res[0])]:   
+    for e, energy in enumerate(e_res):
 
-        model += DCM_lineshape(energy_axis, z[e]*gamma[e], phi[e], energy, gamma[e])
+        model += DCM_lineshape(energy_axis,
+                               z[e]*gamma[e], phi[e], energy, gamma[e])
 
-        model *= energy_axis* lineshape_constant
+        model *= energy_axis * lineshape_constant
 
         model += params[-1]
 
     return model
 
+
 def read_command_line():
     parser = AP()
-    parser.add_argument('-p','--plot', help="show a plot",action='store_true',default=False)
-    parser.add_argument('-i', '--IR_intensity', type=float, help="IR intensity")
+    parser.add_argument('-p', '--plot', help="show a plot",
+                        action='store_true', default=False)
+    parser.add_argument('-i', '--IR_intensity',
+                        type=float, help="IR intensity")
     return vars(parser.parse_args())
+
 
 def moving_average(input_data, window_size):
     window = np.ones(int(window_size))/float(window_size)
     return np.convolve(input_data, window, 'same')
 
-def smooth_data(wdata,nits=8):
+
+def smooth_data(wdata, nits=8):
     out_data = wdata
     for ii in np.arange(nits):
-        out_data=moving_average(out_data,4)
+        out_data = moving_average(out_data, 4)
     return out_data
+
 
 def strip_num(filename):
     """
     Obtain the time delay from the directory name
     """
     import re
-    pat=re.compile("[0-9]{1}.[0-9]{2}")
-    m=pat.search(filename)
-    pat=re.compile("[+,-]")
-    n=pat.search(filename)
+    pat = re.compile("[0-9]{1}.[0-9]{2}")
+    m = pat.search(filename)
+    pat = re.compile("[+,-]")
+    n = pat.search(filename)
     return(float(n.group(0)+m.group(0)))
+
 
 def order_files(filelist):
     """
     Sort the files into increasing order of time delay
     """
     from operator import itemgetter
-    td_list=[]
+    td_list = []
     for fname in filelist:
-        td_list.append((fname,strip_num(fname))) 
+        td_list.append((fname, strip_num(fname)))
 
     return(sorted(td_list, key=itemgetter(1), reverse=True))
+
 
 def grab_data(fname):
     df = pd.read_csv(fname, delim_whitespace=True)
